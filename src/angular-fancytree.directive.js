e39a96a3f847c72d96448b716c8395afb4484450
Tomasz Szymanek <tomasz.szymanek@gmail.com>
2015-12-17 10:16:10 +0100

    removed controller, logic moved to directive

--- src/angular-fancytree.directive.js
+++ src/angular-fancytree.directive.js
@@ -5,18 +5,67 @@
         .module('angular-fancytree')
         .directive('fancytree', fancytree);
 
-    fancytree.$inject = [];
+    fancytree.$inject = ['$timeout', 'fancytreeFactory'];
 
     /* @ngInject */
-    function fancytree() {
+    function fancytree($timeout, fancytreeFactory) {
         var directive = {
-            bindToController: true,
-            controller: "FancyTreeController",
-            controllerAs: 'vm',
-            template: '<div id="fancytree"></div>',
-            restrict: 'EA',
+            template: '<div id="{{ id }}"></div>',
+            restrict: 'EAC',
             scope: {
-                array: '=array'
+                data: "=",
+                id: "@",
+                activevisible: "@",
+                aria: "@",
+                autovctivate: "@",
+                autovollapse: "@",
+                autoscroll: "@",
+                clickfoldermode: "@",
+                checkbox: "@",
+                debuglevel: "@",
+                disabled: "@",
+                focusonselect: "@",
+                generateids: "@",
+                idprefix: "@",
+                icon: "@",
+                keyboard: "@",
+                keypathseparator: "@",
+                minexpandlevel: "@",
+                quicksearch: "@",
+                selectmode: "@",
+                tabbable: "@",
+                titlestabbable: "@"
+            },
+            link: function(scope, element, attrs) {
+                var initializeFancytree = function () {
+                    return $timeout(function () {
+                        var fancytree = $('#' + scope.id);
+                        fancytree.fancytree({
+                            source: fancytreeFactory.getData(),
+                            activeVisible: scope.activevisible === 'true',
+                            aria: scope.aria !== 'false',
+                            autoActivate: scope.autoactivate === 'true',
+                            autoCollapse: scope.autocollapse !== 'false',
+                            autoScroll: scope.autoscroll !== 'false',
+                            clickFolderMode: scope.clickfoldermode != null ? parseInt(scope.clickFolderMode) : 4,
+                            checkbox: scope.checkbox !== 'false',
+                            debugLevel: scope.debuglevel != null ? parseInt(scope.debugLevel) : 2,
+                            disabled: scope.disabled !== 'false',
+                            focusOnSelect: scope.focusonselect !== 'false',
+                            generateIds: scope.generateids !== 'false',
+                            idPrefix: scope.idprefix || "ft_",
+                            icon: scope.icon === 'true',
+                            keyboard: scope.keyboard === 'true',
+                            keyPathSeparator: scope.keypathseparator || "/",
+                            minExpandLevel: scope.minexpandlevel != null ? parseInt(scope.minExpandLevel) : 1,
+                            quicksearch: scope.quicksearch !== 'false',
+                            selectMode: scope.selectmode != null ? parseInt(scope.selectMode) : 2,
+                            tabbable: scope.tabbable === 'true',
+                            titlesTabbable: scope.titlestabbable !== 'false'
+                        });
+                    });
+                };
+                initializeFancytree();
             }
         };
         return directive;
--- src/angular-fancytree.module.js
+++ src/angular-fancytree.module.js
@@ -3,5 +3,4 @@
 
     angular
         .module('angular-fancytree', []);
-
 })();
